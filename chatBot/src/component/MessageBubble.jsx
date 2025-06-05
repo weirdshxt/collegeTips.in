9163753c78ac660ff8b58355608da191e8332771
Kabir Khan <kabirkhann1118@gmail.com>
2025-06-06 04:58:21 +0530

    fix chatBubble

--- chatBot/src/component/MessageBubble.jsx
+++ chatBot/src/component/MessageBubble.jsx
@@ -1,6 +1,6 @@
 const MessageBubble = ({ text, sender }) => {
   const base = "px-4 py-2 max-w-[80%] w-fit rounded-xl text-sm";
-  const user = "bg-violet-700 text-right ml-auto text-[#e2e1ff]";
+  const user = "bg-violet-700 ml-auto text-[#e2e1ff]";
   const bot = "bg-[#e8e6ff] text-left mr-auto text-violet-700";
 
   // If the sender is 'bot', the text might be HTML, so use dangerouslySetInnerHTML.
